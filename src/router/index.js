9719de5fbdc744318e53f3ad7c32fb517b8af0f9
stojic.mate <stojic.mate@nsoft.com>
2021-04-06 15:23:53 +0200

    :seedling: create pages routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,42 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import auth from '@/api/auth';
 
 Vue.use(Router);
 
 const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/pages/Home'),
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: () => import('@/pages/Login'),
+    },
+    {
+      path: '*',
+      redirect: 'login',
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !auth.currentUser()) {
+    next({ path: '/login' });
+  } else if (to.name === 'login' && auth.currentUser()) {
+    next({ path: '/' });
+  } else {
+    next();
+  }
+  next();
 });
 
 export default router;
